959953ab2e1bb1ae438b329452cd9ceffcbd4335
Nadeeshani Senevirathna <nadee158@gmail.com>
2016-11-19 08:11:43 +0530

    modified

---
+++ eventapp/ui-web/src/main/webapp/app/category/categorysetup.js
@@ -0,0 +1,36 @@
+'use strict';
+
+define(['app','nvd3', 'ui_bootstrap'], function (app, nvd3, ui_bootstrap) {
+
+  app.controller('CategorySetupController', ['$scope', '$rootScope', '$log', '$sce', 
+                                          'NotificationServiceFactory',
+                                          'SecurityServiceFactory','Page', 
+                                          'CategoryServiceFactory',
+                                          'CommonServiceFactory',
+                                          'ModalDialogServiceFactory',
+                                  function($scope, $rootScope, $log, $sce, 
+                                		  NotificationServiceFactory,
+                                		  SecurityServiceFactory,Page, 
+                                		  CategoryServiceFactory,
+                                		  CommonServiceFactory,
+                                		  ModalDialogServiceFactory) {
+	  Page.setTitle("Category Setup");
+	  
+	  
+	  $scope.initializeCategorySetupController = function() {
+      	
+      };
+      
+      
+	  
+	  
+   
+   
+ }]);
+  
+  
+
+});
+
+
+
